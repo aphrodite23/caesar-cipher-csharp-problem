ee2c3dabe819d4bbfc8ccdf785ba012453c6f326
pragya.singh <pragya.singh@cgi.com>
2019-08-29 17:45:05 -0700

    changes

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,10 +7,26 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+           
+            string str="";
+            foreach(char ch in text){
+                //spaces,special chars
+                //uppercase
+                //lowercase
+    
+                if(!char.IsLetter(ch)){
+                    str=str+ch;
+                    continue;
+                }
+                char d=(char.IsUpper(ch)?'A':'a');
+                char s = (char)((((ch+shiftKey)-d)%26)+d);
+                str=str+s;
+               }
+               return str;
         }
         public static void Main(string[] args){
             
         }
-    }
+ }
 }
+
